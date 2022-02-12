f135ca29cfd2fc518a4400e0460d05c905e92a5f
Ozimok Aleksey <jambo1ru@yandex.ru>
2022-02-13 00:43:41 +0300

    задача 3

---
+++ 003/Program.cs
@@ -0,0 +1,24 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.Clear();
+double a, b;
+string s;
+Console.Write("Введите первое число а=");
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+Console.Write("Введите второе число b=");
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+
+if (a>b)
+{
+    System.Console.WriteLine("Наибольшее {0}, наименьшее {1}", a, b);
+}
+else if (b>a)
+{
+    System.Console.WriteLine("Наибольшее {0}, наименьшее {1}", b, a);
+}
+else
+{
+    System.Console.WriteLine("Значения {0} и {1} равны", a, b);
+}
