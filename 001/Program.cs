9605b2b680452f2d03afd96379b512339c6da94c
Ozimok Aleksey <jambo1ru@yandex.ru>
2022-02-13 00:31:12 +0300

    задача 1

---
+++ 001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывести квадрат числа
+
+Console.Clear();
+double a, sqw=0;
+string s;
+Console.Write("Введите число, которое необходимо возвести в квадрат а=");
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+
+sqw=a*a;
+System.Console.WriteLine("Квадрат заданного числа: {0}", sqw);
